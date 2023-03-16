f10644737f80771528d11d2d1b5639118d907730
Nagotabimaru <kruz.r@list.ru>
2023-03-16 13:55:25 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import csv
+import json
+import os
+
+
+# 1 задание (тип как-то так ?)
+# text = open("sample.txt", "r")
+#
+# d = dict()
+#
+# for line in text:
+#     line = line.strip()
+#     line = line.lower()
+#     words = line.split(" ")
+#
+#     for word in words:
+#         if word in d:
+#             d[word] = d[word] + 1
+#         else:
+#             d[word] = 1
+#
+# for key in list(d.keys()):
+#     print(key, ":", d[key])
+
+# 2 Задание
+
+# data = csv.reader(open('mydatalist.csv', 'r'))
+# for row in data:
+#     print(row)
+# names = {'Егор': 5, 'Александр': 10}
+# json.dump(names, open('file.txt', 'w', encoding='utf-8'))
+# print(json.load(open('file.txt', 'r', encoding='utf-8')))
+
+# 3 задание
+# Обычное Переименование
+# os.replace("sample.txt", "1-sample.txt")
+
+
